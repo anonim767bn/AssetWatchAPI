883270092b9c60ac2d600f36db49c2f08da93c01
Your Name <you@example.com>
2024-06-12 19:53:15 +0300

    add models.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI
-from src.http_client import CMCHTTPClient
-from src.config import settings
+from src.router import router
 
 
 app = FastAPI()
 
+app.include_router(router)
 
-cmc_client = CMCHTTPClient(
-    base_url='https://pro-api.coinmarketcap.com',
-    api_key=settings.CMC_API_KEY
-)
 
-@app.get("/cryptocurrencies")
-async def get_cryptocurrencies():
-    return await cmc_client.get_listings()
