b641dafe63e6be2d0c589a47730099701e081cbd
Your Name <you@example.com>
2024-06-13 23:22:28 +0300

    update config

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,6 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     CMC_API_KEY: str
+    DATABASE_URL: str = "sqlite:///./test.db"
+    DB_UPDATE_INTERVAL_MINUTES: int = 1
+    TOKEN_SECRET: str
+    TOKEN_EXPIRE_MINUTES: int = 30 * 60 * 7
+    ALGORITHM: str = "HS256"
+    BASE_API_URL: str = 'https://pro-api.coinmarketcap.com'
+    API_AUTHORIZATION_HEADER: str = 'X-CMC_PRO_API_KEY'
 
     model_config = SettingsConfigDict(env_file=".env")
 
