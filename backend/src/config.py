f2bb1df49a6ae3f181e9a9199001e320508ddec4
Your Name <you@example.com>
2024-06-14 10:53:09 +0300

    beautify config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,17 +1,35 @@
+"""
+This module contains the Settings class which is used to manage the settings for the application.
+
+It uses the BaseSettings class from pydantic_settings and SettingsConfigDict for environment variable configuration.
+"""
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+THIRTY = 30
+SIXTY = 60
+SEVEN = 7
+FIVE = 5
 
 
 class Settings(BaseSettings):
+    """
+    This class is used to manage the settings for the application.
+
+    It uses the BaseSettings class from pydantic_settings and
+    SettingsConfigDict for environment variable configuration.
+    """
+
     CMC_API_KEY: str
-    DATABASE_URL: str = "sqlite:///./test.db"
-    DB_UPDATE_INTERVAL_MINUTES: int = 5
+    DATABASE_URL: str = 'sqlite:///./test.db'
+    DB_UPDATE_INTERVAL_MINUTES: int = FIVE
     TOKEN_SECRET: str
-    TOKEN_EXPIRE_MINUTES: int = 30 * 60 * 7
-    ALGORITHM: str = "HS256"
+    TOKEN_EXPIRE_MINUTES: int = THIRTY * SIXTY * SEVEN
+    ALGORITHM: str = 'HS256'
     BASE_API_URL: str = 'https://pro-api.coinmarketcap.com'
     API_AUTHORIZATION_HEADER: str = 'X-CMC_PRO_API_KEY'
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file='.env')
 
 
-settings=Settings()
+settings = Settings()
